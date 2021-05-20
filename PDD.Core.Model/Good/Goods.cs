c373012314d6e6606a79665a3c51c23095b30470
HiJambo <84315686+HiJambo@users.noreply.github.com>
2021-05-20 20:10:34 +0800

    Add files via upload

---
+++ PDD.Core.Model/Good/Goods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PDD.Core.Model
+{
+    public class Goods
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public int Sprice { get; set; }
+        public int Ssell { get; set; }
+        public int repertory { get; set; }
+        public int sort { get; set; }
+        public bool State { get; set; }
+        public DateTime SDate { get; set; }
+
+        public int PhotoId { get; set; }
+        public string DefPhoto { get; set; }
+        public int SpId { get; set; }
+        public int Paid { get; set; }
+        public int Bus_BId { get; set; }
+        public int EId { get; set; }
+        public int FmId { get; set; }
+        public int Uid { get; set; }
+        //新加的字段
+        public int KId { get; set; }
+        public int STeid { get; set; }
+        public int Mcid { get; set; }
+        public int ShopID { get; set; }
+        public int Tid { get; set; }
+    }
+}
---
+++ PDD.Core.Model/OrderDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDD.Core.Model
+{
+   public class OrderDetails
+    {
+        public int OrderId           { get; set; }
+        public int SId               { get; set; }
+        public string OrderNum          { get; set; }
+        public DateTime OrderCreateTime   { get; set; }
+        public string ShippingMay       { get; set; }
+        public int State             { get; set; }
+        public int GoodNum           { get; set; }
+        public double GoodPrefer        { get; set; }
+        public int OrderOperId       { get; set; }
+        public int Uid               { get; set; }
+        public int PTid              { get; set; }
+       
+    }
+}
